74f8308fc0ca94c9d205abc628387bbd16083fe9
EFlaminggo <electricflaminggo@gmail.com>
2024-07-17 15:22:02 +0700

    first commit

---
+++ admin/files/footer.php
@@ -0,0 +1,11 @@
+  <footer class="footer">
+    <div class="container-fluid">
+      <div class="copyright">
+        &copy;
+        <script>
+          document.write(new Date().getFullYear())
+        </script>, Powered By
+        <a href="#" target="_blank">Flaquiz</a>.
+      </div>
+    </div>
+  </footer>
---
+++ database/config.php
@@ -0,0 +1,7 @@
+<?php
+//database configurations
+define("DB_HOST", "localhost");
+define("DB_UNAME", "root");
+define("DB_PASS", "");
+define("DB_DNAME", "quiz_db");
+$conn = mysqli_connect(DB_HOST, DB_UNAME, DB_PASS, DB_DNAME);
